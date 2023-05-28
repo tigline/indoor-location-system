843343f39fb3f9c46e85ee1058771377bab7a2c3
tigline <projectgame@163.com>
2023-05-29 00:33:29 +0900

    modify. across access

--- src/main/kotlin/com/lgguan/iot/position/config/WebMvcConfig.kt
+++ src/main/kotlin/com/lgguan/iot/position/config/WebMvcConfig.kt
@@ -3,6 +3,7 @@ package com.lgguan.iot.position.config
 import com.lgguan.iot.position.aop.AuthInterceptor
 import com.lgguan.iot.position.aop.RequestLogInterceptor
 import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.CorsRegistry
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
@@ -18,5 +19,11 @@ class WebMvcConfig(private val authInterceptor: AuthInterceptor, private val req
         registry.addInterceptor(authInterceptor)
             .excludePathPatterns("/api/v1/register", "/api/v1/login", "/api/v1/refreshToken", "/test/**",
                 "/v3/api-docs/**", "/doc.html", "/swagger-ui/**", "/webjars/**", "/favicon.ico")
+    }
+
+    override fun addCorsMappings(registry: CorsRegistry) {
+        registry.addMapping("/**")
+            .allowedOrigins("http://localhost:8000")
+            .allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS");
     }
 }
